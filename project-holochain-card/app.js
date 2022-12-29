4f01fb426a48c4ed53b46e956170b9fd937e4da8
midCenturyDude7 <mgriffes@gmail.com>
2022-12-28 17:26:24 -0800

    Set up initial draft of interactive card

---
+++ project-holochain-card/app.js
@@ -0,0 +1,20 @@
+const colOne = document.querySelector(".one");
+const colTwo = document.querySelector(".two");
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const paraOne = document.querySelector(".para-one");
+const paraTwo = document.querySelector(".para-two");
+
+second.addEventListener("click", () => {
+  paraOne.style.display = "none";
+  paraTwo.style.display = "block";
+  colOne.style.borderBottom = "none";
+  colTwo.style.borderBottom = "4px solid black";
+});
+
+first.addEventListener("click", () => {
+  paraOne.style.display = "block";
+  paraTwo.style.display = "none";
+  colOne.style.borderBottom = "4px solid black";
+  colTwo.style.borderBottom = "none";
+});
