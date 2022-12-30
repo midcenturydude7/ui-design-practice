718e1f5c115531748c7b07d2b894ba85923813d9
midCenturyDude7 <mgriffes@gmail.com>
2022-12-29 20:09:05 -0800

    Working with CSS animations and js

--- project-holochain-card/app.js
+++ project-holochain-card/app.js
@@ -5,16 +5,39 @@ const second = document.getElementById("second");
 const paraOne = document.querySelector(".para-one");
 const paraTwo = document.querySelector(".para-two");
 
+// The 411
+first.addEventListener("click", () => {
+  paraTwo.style.display = "none";
+  paraOne.classList.replace("para-one-invisible", "clicked-left");
+  colOne.style.borderBottom = "4px solid black";
+  colTwo.style.borderBottom = "none";
+});
+
+// What's Next
 second.addEventListener("click", () => {
-  paraOne.style.display = "none";
+  paraOne.classList.replace("para-one", "para-one-invisible");
   paraTwo.classList.replace("para-two", "clicked-right");
   colOne.style.borderBottom = "none";
   colTwo.style.borderBottom = "4px solid black";
 });
 
-first.addEventListener("click", () => {
-  paraOne.style.display = "block";
-  paraTwo.style.display = "none";
-  colOne.style.borderBottom = "4px solid black";
-  colTwo.style.borderBottom = "none";
-});
+// function infoBtn() {
+//   first.addEventListener("click", () => {
+//     paraTwo.style.display = "none";
+//     paraOne.classList.replace("para-one-invisible", "clicked-left");
+//     colOne.style.borderBottom = "4px solid black";
+//     colTwo.style.borderBottom = "none";
+//   });
+// }
+
+// function nextBtn() {
+//   second.addEventListener("click", () => {
+//     paraOne.classList.replace("para-one", "para-one-invisible");
+//     paraTwo.classList.replace("para-two", "clicked-right");
+//     colOne.style.borderBottom = "none";
+//     colTwo.style.borderBottom = "4px solid black";
+//   });
+// }
+
+// second.onclick = nextBtn();
+// first.onclick = infoBtn();
